5fe94893010ba0443772bc5dd20493e8f8526ee9
wdryck marcos <wendrycksebra92@gmail.com>
2022-07-04 00:11:06 -0500

    exemplo02

---
+++ exemplo02/exemplo02.pas
@@ -0,0 +1,30 @@
+unit exemplo02;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button2: TButton;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
